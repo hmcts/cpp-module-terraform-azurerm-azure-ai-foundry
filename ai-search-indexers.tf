fb5f5a42a606b7bec55248edd21e0ee566f7a847
jyothi.balla <jyothi.balla@hmcts.net>
2025-08-04 17:27:40 +0100

    AI hub and its resources

--- ai-search-indexers.tf
+++ ai-search-indexers.tf
@@ -30,6 +30,6 @@ resource "restapi_object" "ai_search_indexers" {
   data         = jsonencode(local.indexers_json)
   id_attribute = "name" # The ID field on the response
   depends_on = [
-    azurerm_search_service.main, restapi_object.ai_search_indexes, restapi_object.ai_search_skillsets
+    azurerm_search_service.main, restapi_object.ai_search_indexes, restapi_object.ai_search_skillsets, azurerm_role_assignment.search_storage_reader
   ]
 }

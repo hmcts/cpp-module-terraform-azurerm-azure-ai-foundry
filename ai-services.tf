0f622721f84cd38022f5f03930147b9939bbff3e
jyothi.balla <jyothi.balla@hmcts.net>
2025-08-06 17:14:11 +0100

    AI hub and its resources

--- ai-services.tf
+++ ai-services.tf
@@ -61,8 +61,11 @@ resource "azapi_resource" "AIServicesConnection" {
     properties = {
       category      = "AIServices",
       target        = azurerm_ai_services.AIServices.endpoint,
-      authType      = "AAD",
+      authType      = "APIKey",
       isSharedToAll = true,
+      credentials = {
+              key = azurerm_ai_services.AIServices.primary_access_key  # <<<<<< required when using APIKey auth
+            },
       metadata = {
         ApiType    = "Azure",
         ResourceId = azurerm_ai_services.AIServices.id

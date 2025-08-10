9460a8c716a020f67aaac3a82019ff6e59534e03
jyothi.balla <jyothi.balla@hmcts.net>
2025-08-10 16:19:24 +0100

    AI hub and its resources

--- ai-search-datasource.tf
+++ ai-search-datasource.tf
@@ -12,7 +12,7 @@ provider "restapi" {
   debug                = true
 
   headers = {
-    "api-key"      = var.lookup_search_service ? data.azurerm_search_service.search[0].primary_key : "",
+    "api-key"      = azurerm_search_service.main.primary_key,
     "Content-Type" = "application/json"
   }
 }

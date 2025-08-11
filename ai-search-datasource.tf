16562b9c57f9eff50b65dbb210f7f739d80c5daf
jyothi.balla <jyothi.balla@hmcts.net>
2025-08-11 09:51:26 +0100

    AI hub and its resources

--- ai-search-datasource.tf
+++ ai-search-datasource.tf
@@ -17,7 +17,7 @@ provider "restapi" {
   }
 }
 
-
+#for every run it gets triggered, just run only when there is a change in json -- needs to be fixed
 resource "restapi_object" "storage_account_datasource" {
   path         = "/datasources"
   query_string = "api-version=2023-10-01-Preview"

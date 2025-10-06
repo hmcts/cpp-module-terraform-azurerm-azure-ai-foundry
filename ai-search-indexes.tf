d6ebda3e9ba2e0c18a4964d66f24c9dfe4315087
jyothi.balla <jyothi.balla@hmcts.net>
2025-10-06 17:33:05 +0100

    AI hub and its resources

--- ai-search-indexes.tf
+++ ai-search-indexes.tf
@@ -9,23 +9,23 @@ data "http" "ai_search_index_json" {
   url = "https://raw.githubusercontent.com/hmcts/cp-ai-rag-service/main/ai-document-shared-artefacts/src/main/resources/vector-db-index-schema.json"
 }
 
-# provider "restapi" {
-#   uri                  = "https://${azurerm_search_service.main.name}.search.windows.net"
-#   write_returns_object = true
-#   debug                = true
-#
-#   headers = {
-#     "api-key"      = azurerm_search_service.main.primary_key,
-#     "Content-Type" = "application/json"
-#   }
-# }
-#
-# resource "restapi_object" "ai_search_indexes" {
-#   path         = "/indexes"
-#   query_string = "api-version=2025-05-01-preview"
-#   data         = data.http.ai_search_index_json.response_body
-#   id_attribute = "name" # The ID field on the response
-#   depends_on = [
-#     azurerm_search_service.main, azurerm_private_endpoint.ai_search_pe
-#   ]
-# }
+provider "restapi" {
+  uri                  = "https://${azurerm_search_service.main.name}.search.windows.net"
+  write_returns_object = true
+  debug                = true
+
+  headers = {
+    "api-key"      = azurerm_search_service.main.primary_key,
+    "Content-Type" = "application/json"
+  }
+}
+
+resource "restapi_object" "ai_search_indexes" {
+  path         = "/indexes"
+  query_string = "api-version=2025-05-01-preview"
+  data         = data.http.ai_search_index_json.response_body
+  id_attribute = "name" # The ID field on the response
+  depends_on = [
+    azurerm_search_service.main, azurerm_private_endpoint.ai_search_pe
+  ]
+}
